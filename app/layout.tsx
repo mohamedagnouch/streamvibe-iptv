78318b97004acf89e11cd3becdccea3eaee98a22
agnouch mohamed <agnouchmohamed24@gmail.com>
2026-01-26 11:27:58 +0100

    chore: update Next.js to fix security vulnerability

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,23 +30,20 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4M41N3ESZ0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-              `}
-            </Script>
-          </>
-        )}
+            gtag('config', 'G-4M41N3ESZ0');
+          `}
+        </Script>
+
         {children}
         <ScrollToTop />
       </body>
